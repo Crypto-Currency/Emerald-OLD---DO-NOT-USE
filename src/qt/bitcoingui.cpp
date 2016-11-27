8962042d1889bd3155b45aa37e3bc25e7ec37435
Spidersbox <Spider@Spidersbox.com>
2016-11-27 14:22:49 -0800

    another option ...

--- src/qt/bitcoingui.cpp
+++ src/qt/bitcoingui.cpp
@@ -69,8 +69,8 @@ BitcoinGUI::BitcoinGUI(QWidget *parent) :
     setWindowTitle(tr("Emerald") + " - " + tr("Wallet")+" "+QString::fromStdString(CLIENT_BUILD));
 //setStyleSheet("QMainWindow{background-color:rgb(38,139,65)} QToolButton:!hover{background-color:rgb(10,90,32); color:black;border-style:outset; border-width:2px; border-color:darkgrey; border-radius:10px}  QToolButton:hover{background-color:rgb(255,213,132); color:black; border-style: outset; border-width: 2px; border-radius: 10px; border-color: darkgrey} QToolTip {background-color:rgb(255,233,142); color:black; border: 2px solid grey;};color black");
 
-setStyleSheet("QMainWindow{background: url(:images/background) 0 0 0 0 stretch stretch;}");
-
+setStyleSheet("border-image: url(:images/background) 0 0 0 0 stretch stretch;");
+//setStyleSheet("background-image: url(:/images/background)  0 0 0 0 stretch stretch;");
 #ifndef Q_OS_MAC
     QApplication::setWindowIcon(QIcon(":icons/bitcoin"));
     setWindowIcon(QIcon(":icons/bitcoin"));
