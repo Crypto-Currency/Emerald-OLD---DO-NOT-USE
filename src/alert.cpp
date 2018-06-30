60234dcccaf64d538cfcd89200045ce0f1a76e14
Spidersbox <Spider@Spidersbox.com>
2018-06-30 12:20:25 -0700

    rem out for boot 1.49

--- src/alert.cpp
+++ src/alert.cpp
@@ -4,7 +4,7 @@
 
 #include <algorithm>
 //#include <boost/algorithm/string/classification.hpp>
-//#include <boost/algorithm/string/replace.hpp>
+#include <boost/algorithm/string/replace.hpp>
 #include <boost/foreach.hpp>
 #include <map>
 
